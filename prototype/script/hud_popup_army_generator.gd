19babb50c66a1cedb9e78dd04c624476de08f4e5
andres <andres@andres-H61ML>
2021-11-04 10:27:41 -0300

    Actualizar hud.tscn y hud_popup_army_generator.gd

---
+++ prototype/script/hud_popup_army_generator.gd
@@ -0,0 +1,11 @@
+extends WindowDialog
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	popup()
+
+func _on_Button_add_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_Button_less_pressed() -> void:
+	pass # Replace with function body.
