1f4e37da5773db6fbefeb5b6150a3405dee6cda7
andresTapari <andrestapari@gmail.com>
2021-11-06 11:17:56 -0300

    Actualizar UNIT_CLASESS.gd

--- prototype_tapa/script/globals/UNIT_CLASESS.gd
+++ prototype_tapa/script/globals/UNIT_CLASESS.gd
@@ -7,21 +7,21 @@ enum classes {	triangulo, 	#0
 
 # Definición de unidades:
 var triangulo = {"Type": classes.triangulo,
-				 "Health": 10,
+				 "Health": 15,
 				 "Damage": 2,
 				 "Armor": 5,
 				 "Speed":5
 				}
 
-var cubo = 		{"Type": classes.cubo
-				,"Health": 20,
+var cubo = 		{"Type": classes.cubo,
+				"Health": 20,
 				"Damage": 1, 
 				"Armor": 5, 
-				"Speed": 2 }
+				"Speed": 2.5 }
 
 var esfera = 	{"Type": classes.esfera,
 				 "Health": 5,
-				 "Damage": 7,
+				 "Damage": 10,
 				 "Armor": 0,
-				 "Speed":6
+				 "Speed":7.5
 				}
