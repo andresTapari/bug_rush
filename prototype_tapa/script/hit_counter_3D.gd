0d953e8faed00254192737ee9ba61548d9f0ebc3
andres <andres@andres-H61ML>
2021-11-10 12:46:54 -0300

    Actualizar dev_lvl_combat.tscn, lvl_selection.tscn y 3 más archivos...

--- prototype_tapa/script/hit_counter_3D.gd
+++ prototype_tapa/script/hit_counter_3D.gd
@@ -2,10 +2,6 @@ extends Spatial
 
 var vel = 5
 
-func _ready() -> void:
-	pass
-#	$Viewport/hit_counter_2D/Label.visible = true
-
 func _process(_delta) -> void:
 	transform.origin += vel * Vector3(0,1,0) * _delta
 
@@ -15,5 +11,5 @@ func set_damage(_value: int) -> void:
 
 func _on_Timer_timeout() -> void:
 	queue_free()
-#	pass
+
 	
