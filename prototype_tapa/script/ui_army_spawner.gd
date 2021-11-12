f6c23f0cee3c7bf09499e609d1eb75ef0aed8243
Andres <Andres@DESKTOP-AQA2GOF.local>
2021-11-12 20:39:26 -0300

    Actualizar ui_army_spawner.tscn, ui_army_unit_button.tscn y ui_army_spawner.gd

--- prototype_tapa/script/ui_army_spawner.gd
+++ prototype_tapa/script/ui_army_spawner.gd
@@ -12,3 +12,13 @@ func _ready() -> void:
 
 func handle_update_coins_counter():
 	label_coins.text = String(LVL_MASTER.player_info["coins"])
+
+
+func _on_Button_pressed():
+	var army_list: Array = Array()
+	var units_btn_list = unit_list.get_children()
+	for item in units_btn_list:
+		if item is MarginContainer:
+			var value = item.get_unit_info()
+			army_list.push_back(value)
+	print(army_list)
