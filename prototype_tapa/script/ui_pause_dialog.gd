e26da6fb7b764195d9fc67870778eae50ffab90b
andres <andres@andres-H61ML>
2021-11-26 11:53:01 -0300

    Actualizar ui_pause_dialog.tscn y ui_pause_dialog.gd

--- prototype_tapa/script/ui_pause_dialog.gd
+++ prototype_tapa/script/ui_pause_dialog.gd
@@ -11,18 +11,22 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Button_main_menu_pressed():
+	SOUND_FX.button_play("back/cancel")
 	var lvl_selection = "res://lvls/main_menu.tscn"
 	#warning-ignore:return_value_discarded
 	get_tree().change_scene(lvl_selection)
 
 
 func _on_Button_lvl_selection_pressed():
+	SOUND_FX.button_play("acept")
 	var lvl_selection = "res://lvls/lvl_selection.tscn"
 	#warning-ignore:return_value_discarded
 	get_tree().change_scene(lvl_selection)
 
 func _on_Button_back_pressed():
+	SOUND_FX.button_play("back/cancel")
 	hide()
 
 func _on_Button_setup_pressed() -> void:
+	SOUND_FX.button_play("acept")
 	$Setup_dialog.popup()
